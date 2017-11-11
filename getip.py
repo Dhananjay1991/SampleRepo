0798736fad490a5864e94017380d3ad3b2f54e74
Dhananjay1991 <chikucool.22@gmail.com>
2017-11-12 00:37:24 +0530

    uncomment public ip

--- getip.py
+++ getip.py
@@ -1,8 +1,8 @@
-# import socket   
-# hostname=socket.gethostname()   
-# print hostname
-# IPAddr=socket.gethostbyname(hostname)   
-# print("Your Computer Name is:"+hostname)   
+import socket   
+hostname=socket.gethostname()   
+print hostname
+IPAddr=socket.gethostbyname(hostname)   
+print("Your Computer Name is:"+hostname)   
 # print("Your Computer IP Address is:"+IPAddr)  
 
 import socket
